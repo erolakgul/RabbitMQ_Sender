7a63b4843264bc2b3779257b7059468e2ae04e2e
EROL AKGÜL <erolakgul88@gmail.com>
2021-11-15 17:41:59 +0300

    Add project files.

---
+++ RabbitMQ_Sender/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+// https://csharp.hotexamples.com/examples/RabbitMQ.Client/ConnectionFactory/-/php-connectionfactory-class-examples.html
+namespace RabbitMQ_Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost"  }; //, Port = 15672
+
+            //IConnection conn = factory.CreateConnection();
+            //IModel ch = conn.CreateModel();
+            //ch.Close(Constants.ReplySuccess, "Closing the channel"); 
+            //conn.Close(Constants.ReplySuccess, "Closing the connection");
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
